e1e3f09a0a945079137adf7d8cff130d4562bfcb
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-03-15 23:47:39 +0200

    Create EmailEntityCreatedEvent.cs

---
+++ src/Core/Domain/Email/Events/EmailEntityCreatedEvent.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Email.Entities;
+
+namespace Domain.Email.Events;
+
+public class EmailEntityCreatedEvent(EmailEntity emailEntity) : Event
+{
+    public EmailEntity EventEntity { get; set; } = emailEntity;
+}
