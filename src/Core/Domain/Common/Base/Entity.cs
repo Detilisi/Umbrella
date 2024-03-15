8965b6955de86c758c81b1d85a92c901fa473625
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-03-15 22:33:58 +0200

    Add base objects for domain layer

---
+++ src/Core/Domain/Common/Base/Entity.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Common.Base;
+
+public abstract class Entity
+{
+    //Fields
+    private readonly List<Event> _domainEvents = new();
+    [NotMapped] public IReadOnlyCollection<Event> DomainEvents => _domainEvents.AsReadOnly();
+
+    //Properties
+    public int Id { get; protected set; }
+    public DateTime ModifiedAt { get; set; }
+    public DateTime CreatedAt { get; protected set; }
+
+    //Construction
+    public Entity()
+    {
+        //Required for Entity framework
+    }
+    public Entity(int id)
+    {
+        Id = id;
+        CreatedAt = DateTime.UtcNow;
+    }
+
+    //Event methods
+    public void ClearDomainEvents() => _domainEvents.Clear();
+    public void AddDomainEvent(Event domainEvent) => _domainEvents.Add(domainEvent);
+    public void RemoveDomainEvent(Event domainEvent) => _domainEvents.Remove(domainEvent);
+    
+    //Overide methods
+    public override int GetHashCode() => Id.GetHashCode();
+    public override bool Equals(object? obj) { return obj is Entity entity && Id.Equals(entity.Id); }
+}
---
+++ src/Core/Domain/Common/Base/Event.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Domain.Common.Base;
+
+public abstract class Event : INotification
+{
+    public DateTime DateOccurred { get; protected set; } = DateTime.UtcNow;
+}
