103e5a3ee9eed95abae3effcceaf5feb18fa041f
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-03-16 19:08:04 +0200

    Create PaginatedList.cs

---
+++ src/Common/Shared/Common/Lists/PaginatedList.cs
@@ -0,0 +1,26 @@
+﻿namespace Shared.Common.Lists;
+
+public class PaginatedList<T> where T : class
+{
+    //Properties
+    public List<T> Items { get; }
+
+    public int PageSize { get; }
+    public int TotalPages { get; }
+    public int TotalCount { get; }
+    public int CurrentPage { get; set; }
+
+    public bool HasPreviousPage => CurrentPage > 1;
+    public bool HasNextPage => CurrentPage < TotalPages;
+
+    //Construction
+    public PaginatedList(List<T> items, int count, int currentPage, int pageSize =10)
+    {
+        Items = items;
+        TotalCount = count;
+        CurrentPage = currentPage;
+        PageSize = pageSize;
+        
+        TotalPages = (int)Math.Ceiling(TotalCount / (double)PageSize);
+    }
+}
