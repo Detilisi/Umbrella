040bcb7f411b8e4abc94e6b7ef87e472c181e2fb
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-03-16 16:37:53 +0200

    Create UserEntity.cs

---
+++ src/Core/Domain/User/Entities/UserEntity.cs
@@ -0,0 +1,38 @@
+﻿using Domain.User.Events;
+using Domain.User.ValueObjects;
+
+namespace Domain.User.Entities;
+
+public class UserEntity : Entity
+{
+    //Properties
+    public EmailAddress EmailAddress { get; private set; }
+    public EmailPassword EmailPassword { get; private set; }
+    public string UserName { get; private set; } = string.Empty;
+
+    //Construction
+    private UserEntity
+    (
+        int id, 
+        EmailAddress emailAddress, 
+        EmailPassword password, 
+        string username
+    ) : base(id)
+    {
+        UserName = username;
+        EmailPassword = password;
+        EmailAddress = emailAddress;
+
+        AddDomainEvent(new UserEntityCreatedEvent(this));
+    }
+
+    public static UserEntity Create
+    (
+        EmailAddress emailAddress, 
+        EmailPassword password, 
+        string username
+    )
+    {
+        return new UserEntity(0, emailAddress, password, username);
+    }
+}
