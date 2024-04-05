d82f9401ee54020cce987ddf880602bedda4597f
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-04-05 21:33:12 +0200

    Add Maui persistence

---
+++ src/External/EFMigrator/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using MauiPersistance.Services;
+
+Console.WriteLine("Migrator running..");
+
+using (var blogContext = new LocalDatabase())
+{
+    var all = blogContext.Authors.ToList();
+}
