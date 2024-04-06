b5bf4cb087b27f84048bd31b47b97a89e58e04b7
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-04-06 21:03:57 +0200

    Remove old Persistence project

--- src/External/Persistence/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-﻿global using MediatR;
-global using Domain.User.Entities;
-global using Domain.Email.Entities;
-global using Domain.User.ValueObjects;
-global using Microsoft.EntityFrameworkCore;
-global using Microsoft.EntityFrameworkCore.Metadata.Builders;
