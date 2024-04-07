729bebe43fd4cb728b5d02695612e566a7035802
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-04-07 09:55:19 +0200

    Add abstractions

---
+++ src/Core/Application/Common/Abstractions/DataContexts/IApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Email.Entities;
+using Domain.User.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Common.Abstractions.DataContexts;
+
+public interface IApplicationDbContext
+{
+    //Data sets
+    DbSet<UserEntity> Users { get; }
+    DbSet<EmailEntity> Emails { get; }
+
+}
