a4c7fbe247dd562a36417073759d3fa8fd16b237
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-04-13 19:40:04 +0200

    Add new RegisterUserCommand

--- src/Core/Application/GlobalUsings.cs
+++ src/Core/Application/GlobalUsings.cs
@@ -2,3 +2,4 @@
 global using Domain.Email.Entities.Enums;
 global using Domain.Email.ValueObjects;
 global using Application.Common.Base;
+global using MediatR;
--- src/Core/Application/User/Models/UserModel.cs
+++ src/Core/Application/User/Models/UserModel.cs
@@ -1,10 +1,8 @@
-﻿using Domain.User.ValueObjects;
-
-namespace Application.User.Models;
+﻿namespace Application.User.Models;
 
 public class UserModel : Model
 {
     public string UserName { get; set; } = string.Empty;
-    public required EmailAddress EmailAddress { get; set; }
-    public required EmailPassword EmailPassword { get; set; }
+    public string EmailAddress { get; set; } = string.Empty;
+    public string EmailPassword { get; set; } = string.Empty;
 }
