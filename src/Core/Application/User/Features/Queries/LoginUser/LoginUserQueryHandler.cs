b2da6d9ae420aa2438f3a8187fbbb35eb38bcbdf
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-04-16 17:07:53 +0200

    Update LoginUserQueryHandler.cs

--- src/Core/Application/User/Features/Queries/LoginUser/LoginUserQueryHandler.cs
+++ src/Core/Application/User/Features/Queries/LoginUser/LoginUserQueryHandler.cs
@@ -15,11 +15,11 @@ public class LoginUserQueryHandler(IApplicationDbContext dbContext) : IRequestHa
         var userEntity = await _dbContext.Users.Where(x => x.EmailAddress.Address == request.EmailAddress)
             .FirstOrDefaultAsync(cancellationToken);
 
-        if (userEntity == null) return Result.Failure<UserModel>(AuthenticationErrors.InvalidEmailOrPassword);
+        if (userEntity == null) return Result.Failure<UserModel>(AuthenticationErrors.EmailInvalid);
         
         //Validate password
         var passwordValid = userEntity.EmailPassword.Key.Equals(request.EmailPassword);
-        if (!passwordValid) return Result.Failure<UserModel>(AuthenticationErrors.InvalidEmailOrPassword);
+        if (!passwordValid) return Result.Failure<UserModel>(AuthenticationErrors.PasswordInvalid);
         
 
         var userModel = UserModel.CreateFromUserEntity(userEntity);
