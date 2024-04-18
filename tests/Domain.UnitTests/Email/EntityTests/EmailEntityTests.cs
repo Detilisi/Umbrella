320187c8a944c5d0e8b52292c98e612d415cd457
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-04-18 20:34:20 +0200

    Update EmailEntityTests.cs

--- tests/Domain.UnitTests/Email/EntityTests/EmailEntityTests.cs
+++ tests/Domain.UnitTests/Email/EntityTests/EmailEntityTests.cs
@@ -7,37 +7,49 @@ namespace Domain.UnitTests.Email.EntityTests;
 public class EmailEntityTests
 {
     [Test]
+    public void Create_ThrowsArgumentNullException_ForNullSender()
+    {
+        Assert.Throws<ArgumentNullException>(() =>
+        EmailEntity.Create(null, [EmailAddress.Create("test@example.com")], EmailSubjectLine.Create("Subject"), EmailBodyText.Create("Body")));
+    }
+
+    [Test]
     public void Create_ThrowsArgumentNullException_ForNullRecipients()
     {
-        Assert.Throws<ArgumentNullException>(() => EmailEntity.Create(null, EmailSubjectLine.Create("Subject"), EmailBodyText.Create("Body")));
+        Assert.Throws<ArgumentNullException>(() => 
+        EmailEntity.Create(EmailAddress.Create("test@example.com"), null, EmailSubjectLine.Create("Subject"), EmailBodyText.Create("Body")));
     }
 
     [Test]
     public void Create_ThrowsArgumentNullException_ForNullSubject()
     {
-        Assert.Throws<ArgumentNullException>(() => EmailEntity.Create([EmailAddress.Create("test@example.com")], null, EmailBodyText.Create("Body")));
+        Assert.Throws<ArgumentNullException>(() => 
+        EmailEntity.Create(EmailAddress.Create("test@example.com"), [EmailAddress.Create("test@example.com")], null, EmailBodyText.Create("Body")));
     }
 
     [Test]
     public void Create_ThrowsArgumentNullException_ForNullBody()
     {
-        Assert.Throws<ArgumentNullException>(() => EmailEntity.Create([EmailAddress.Create("test@example.com")], EmailSubjectLine.Create("Subject"), null));
+        Assert.Throws<ArgumentNullException>(() => 
+        EmailEntity.Create(EmailAddress.Create("test@example.com"), [EmailAddress.Create("test@example.com")], EmailSubjectLine.Create("Subject"), null));
     }
 
     [Test]
     public void Create_SetsProperties_ForValidArguments()
     {
         //Arrange
+        var sender = EmailAddress.Create("test@example.com");
         var recipients = new List<EmailAddress>() { EmailAddress.Create("test@example.com") };
         var subject = EmailSubjectLine.Create("This is a subject");
         var body = EmailBodyText.Create("This is the email body");
 
         //Act
-        var email = EmailEntity.Create(recipients, subject, body);
+        var email = EmailEntity.Create(sender, recipients, subject, body);
 
         //Assert
         Assert.Multiple(() =>
         {
+            Assert.That(email.Sender, Is.EqualTo(sender));
             Assert.That(email.Recipients, Is.EqualTo(recipients));
             Assert.That(email.Subject, Is.EqualTo(subject));
             Assert.That(email.Body, Is.EqualTo(body));
@@ -50,12 +62,13 @@ public class EmailEntityTests
     public void Create_AddEmailEntityCreatedEvent_ForValidArguments()
     {
         //Arrange
+        var sender = EmailAddress.Create("test@example.com");
         var recipients = new List<EmailAddress>() { EmailAddress.Create("test@example.com") };
         var subject = EmailSubjectLine.Create("This is a subject");
         var body = EmailBodyText.Create("This is the email body");
 
         //Act
-        var email = EmailEntity.Create(recipients, subject, body);
+        var email = EmailEntity.Create(sender, recipients, subject, body);
 
         //Assert
         Assert.That(email.DomainEvents, Has.Count.EqualTo(1));
