e1c72db3509084317c0d4953ebb897ad242fed67
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-04-18 21:01:17 +0200

    Create UpdateEmailCommandHandler.cs

---
+++ src/Core/Application/Email/Features/Commands/UpdateEmail/UpdateEmailCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Application.Email.Features.Commands.UpdateEmail;
+
+public class UpdateEmailCommandHandler(IApplicationDbContext dbContext) : IRequestHandler<UpdateEmailCommand, Result<int>>
+{
+    //Fields
+    private readonly IApplicationDbContext _dbContext = dbContext;
+
+    //Handle methods
+    public async Task<Result<int>> Handle(UpdateEmailCommand request, CancellationToken cancellationToken)
+    {
+        var emailEntity = EmailEntity.Create
+        (
+           EmailAddress.Create(request.Sender),
+           request.Recipients,
+           EmailSubjectLine.Create(request.Subject),
+           EmailBodyText.Create(request.Body)
+        );
+
+        _dbContext.Emails.Update(emailEntity);
+        await _dbContext.SaveChangesAsync(cancellationToken);
+
+        return Result.Success(emailEntity.Id);
+    }
+}
