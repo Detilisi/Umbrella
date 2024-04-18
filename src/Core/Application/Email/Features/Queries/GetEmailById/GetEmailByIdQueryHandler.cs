cc1d003bd06dabc85534612c2c7fbbffbb685ae0
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-04-18 21:09:50 +0200

    Create GetEmailByIdQueryHandler.cs

---
+++ src/Core/Application/Email/Features/Queries/GetEmailById/GetEmailByIdQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using Application.User.Errors;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Email.Features.Queries.GetEmailById;
+
+public class GetEmailByIdQueryHandler(IApplicationDbContext dbContext) : IRequestHandler<GetEmailByIdQuery, Result<EmailModel>>
+{
+    //Fields
+    private readonly IApplicationDbContext _dbContext = dbContext;
+
+    //Handle method
+    public async Task<Result<EmailModel>> Handle(GetEmailByIdQuery request, CancellationToken cancellationToken)
+    {
+        var emailEntity = await _dbContext.Emails.Where(x => x.Id == request.EmailId).FirstOrDefaultAsync(cancellationToken);
+        if (emailEntity == null) return Result.Failure<EmailModel>(AuthenticationErrors.EmailInvalid);
+
+        var emailModel = EmailModel.CreateFromEntity(emailEntity);
+        return Result.Success(emailModel);
+    }
+}
