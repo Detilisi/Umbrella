75883ef84580b9d8faa210c8897be244a4ea8f01
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-04-22 22:56:58 +0200

    Add EmailFetcher

--- src/External/Infrastructure/GlobalUsings.cs
+++ src/External/Infrastructure/GlobalUsings.cs
@@ -1,2 +1,4 @@
 ﻿global using Shared.Common.Results;
+global using Infrastructure.Email.Extensions;
+global using Infrastructure.Email.Settings;
 global using Application.Common.Abstractions.Services;
