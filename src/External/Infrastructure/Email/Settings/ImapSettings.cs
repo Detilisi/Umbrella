7400e3d0d529d9b7c2277541a3546ea95a63f40e
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-04-22 22:03:22 +0200

    Update ImapSettings.cs

--- src/External/Infrastructure/Email/Settings/ImapSettings.cs
+++ src/External/Infrastructure/Email/Settings/ImapSettings.cs
@@ -10,14 +10,14 @@ internal class ImapSettings(string provider, string server, int port, bool useSs
     public static readonly ImapSettings Outlook = new("Outlook", "outlook.office365.com", 993, true);
 
     //Helper methods
-    public static ImapSettings FindServerSettings(string emailDomain)
+    public static Result<ImapSettings> FindServerSettings(string emailDomain)
     {
         return emailDomain.ToLower() switch
         {
-            "gmail.com" => Gmail,
-            "yahoo.com" => Yahoo,
-            "outlook.com" or "office365.com" => Outlook,
-            _ => throw new NotSupportedException($"Email provider for domain '{emailDomain}' is not supported."),
+            "gmail.com" => Result.Success(Gmail),
+            "yahoo.com" => Result.Success(Yahoo),
+            "outlook.com" or "office365.com" => Result.Success(Outlook),
+            _ => Result.Failure<ImapSettings>(new Error("EmailDomainUnsupported", "Email provider for domain '{emailDomain}' is not supported"))
         };
     }
 }
