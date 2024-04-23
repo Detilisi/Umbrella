8215ae781370e76a34da996dbac6d02e781a2a75
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-04-23 21:41:09 +0200

    Update GlobalUsings.cs

--- src/External/Infrastructure/GlobalUsings.cs
+++ src/External/Infrastructure/GlobalUsings.cs
@@ -1,4 +1,7 @@
 ﻿global using Shared.Common.Results;
 global using Infrastructure.Email.Extensions;
 global using Infrastructure.Email.Settings;
+global using Application.Email.Models;
+global using Application.User.Models;
 global using Application.Common.Abstractions.Services;
+global using Application.Email.Abstractions.Services;
