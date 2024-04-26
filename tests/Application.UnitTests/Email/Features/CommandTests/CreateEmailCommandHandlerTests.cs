73d49fcf6caeb8f9cd072639b8113197169a6313
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-04-26 23:02:27 +0200

    Update CreateEmailCommandHandlerTests.cs

--- tests/Application.UnitTests/Email/Features/CommandTests/CreateEmailCommandHandlerTests.cs
+++ tests/Application.UnitTests/Email/Features/CommandTests/CreateEmailCommandHandlerTests.cs
@@ -1,5 +1,6 @@
 using Application.Email.Features.Commands.CreateEmail;
 using Application.UnitTests.Email.Features.Base;
+using Domain.Common.Exceptions;
 
 namespace Application.UnitTests.Email.Features.Commands;
 
@@ -26,5 +27,21 @@ public class CreateEmailCommandHandlerTests : BaseHandlerTest
         Assert.That(result.IsSuccess, Is.True);
         MockDbContext.Verify(db => db.Emails.Add(It.IsAny<EmailEntity>()), Times.Once);
         MockDbContext.Verify(db => db.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
+    }
+
+    [Test]
+    public void Handle_Should_ThrowArgumentNullException_When_Sender_IsNull()
+    {
+        var handler = new CreateEmailCommandHandler(MockDbContext.Object);
+        var command = new CreateEmailCommand
+        {
+            Sender = null,
+            Recipients = ["recipient1@example.com", "recipient2@example.com"],
+            Subject = "Test Subject",
+            Body = "Test Body"
+        };
+
+        //Act & Assert
+        Assert.ThrowsAsync<EmptyValueException>(async () => await handler.Handle(command, CancellationToken.None));
     }
 }
