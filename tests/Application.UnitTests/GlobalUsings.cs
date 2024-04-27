2027fe9d16a420654a4edb1e95dc66c7983f2dec
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-04-27 19:43:27 +0200

    Remove tests

--- tests/Application.UnitTests/GlobalUsings.cs
+++
@@ -1,7 +0,0 @@
-﻿global using Moq;
-global using Microsoft.EntityFrameworkCore;
-
-global using Domain.User.Entities;
-global using Domain.Email.Entities;
-global using Application.Common.Abstractions.DataContexts;
-
--- tests/Domain.UnitTests/GlobalUsings.cs
+++
@@ -1,4 +0,0 @@
-global using Domain.Common.Exceptions;
-global using Domain.Common.ValueObjects;
-global using Domain.Email.Exceptions;
-global using Domain.Email.ValueObjects;
