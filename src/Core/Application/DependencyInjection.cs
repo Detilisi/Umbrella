af052d0e93baa74b66890dcc66cae4150e79fa06
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-04-29 22:55:07 +0200

    Update DependencyInjection.cs

--- src/Core/Application/DependencyInjection.cs
+++ src/Core/Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using MauiClientApp.Email.EmailList.ViewModels;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Application;
@@ -8,6 +9,7 @@ public static class DependencyInjection
     public static IServiceCollection AddApplicationLayer(this IServiceCollection services)
     {
         services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        services.AddSingleton<EmailListViewModel>();
 
         return services;
     }
