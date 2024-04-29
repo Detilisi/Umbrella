2a6177c44828bbac91c2fc341b31532bed9c2e1a
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-04-29 20:52:53 +0200

    Create ViewModel.cs

---
+++ src/UI/ClientMauiApp/Common/Base/ViewModel.cs
@@ -0,0 +1,17 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Diagnostics;
+
+namespace MauiClientApp.Common.Base;
+
+public abstract partial class ViewModel : ObservableObject
+{
+    //ViewModel lifecylce
+    public virtual void OnViewModelStarting(CancellationToken cancellationToken = default)
+    {
+        Debug.WriteLine($"{nameof(ViewModel)} is starting");
+    }
+    public virtual void OnViewModelClosing(CancellationToken cancellationToken = default)
+    {
+        Debug.WriteLine($"{nameof(ViewModel)} is closing");
+    }
+}
