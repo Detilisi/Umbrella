a958c1c1690830a6cf65bd81b3dc0a3e6bd0dab6
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-04-30 20:54:15 +0200

    Update EmailListViewModel.cs

--- src/UI/ClientMauiApp/Email/EmailList/ViewModels/EmailListViewModel.cs
+++ src/UI/ClientMauiApp/Email/EmailList/ViewModels/EmailListViewModel.cs
@@ -1,12 +1,28 @@
-﻿namespace MauiClientApp.Email.EmailList.ViewModels;
+﻿using Application.Email.Features.Queries.GetEmailList;
 
-public class EmailListViewModel(ViewModel chatViewModel) : EmailViewModel(chatViewModel)
+namespace MauiClientApp.Email.EmailList.ViewModels;
+
+public class EmailListViewModel(ViewModel childViewModel, IMediator mediator) : EmailViewModel(childViewModel)
 {
+    //Fields
+    private IMediator _mediator = mediator;
+
+    //Properties
     public ObservableCollection<EmailModel> EmailMessageList { get; set; } = [];
 
-    public override void OnViewModelStarting(CancellationToken cancellationToken = default)
+    //Life cycle
+    public override async void OnViewModelStarting(CancellationToken cancellationToken = default)
     {
         base.OnViewModelStarting(cancellationToken);
+
+        //load emails
+        var userId = 1;
+        var loadEmailQuery = new GetEmailListQuery(userId);
+
+        var emailList = await _mediator.Send(loadEmailQuery);
+        if (emailList.IsFailure) return;
+
+        //EmailMessageList = emailList.;
     }
 }
 
