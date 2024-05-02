4e9b6c465501b10d5b89cbd003e7d120183a5824
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-05-02 22:36:50 +0200

    Update MauiClientApp.csproj

--- src/UI/ClientMauiApp/Email/Base/Pages/EmailPage.cs
+++ src/UI/ClientMauiApp/Email/Base/Pages/EmailPage.cs
@@ -1,21 +1,15 @@
 ﻿namespace Umbrella.Maui.Email.Base.Pages;
 
-public abstract class EmailPage<TViewModel> : Page<TViewModel> where TViewModel : EmailViewModel 
+public abstract class EmailPage<TViewModel>(TViewModel viewModel, View chatHistoryView) : 
+    Page<TViewModel>(viewModel) where TViewModel : EmailViewModel 
 {
     //Fields
     private enum Row { Content = 0, ChatBox = 1 }
 
     //View components
-    protected View ChatHistory { set; get; }
+    protected View ChatHistory { set; get; } = chatHistoryView;
     protected abstract ScrollView PageContent { get; }
     protected Grid MainGridLayout { get; set; } = null!;
-
-    //Construction
-    protected EmailPage(string title, TViewModel viewModel, View chatHistoryView) : base(viewModel)
-    {
-        Title = title;
-        ChatHistory = chatHistoryView;
-    }
 
     //Initialization
     protected override void OnAppearing()
