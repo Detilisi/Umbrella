5ee602da272b955c636351872f4c80b933f09589
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-05-03 23:15:57 +0200

    Add font awesome icons

--- src/UI/ClientMauiApp/MauiProgram.cs
+++ src/UI/ClientMauiApp/MauiProgram.cs
@@ -16,6 +16,7 @@ public static class MauiProgram
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                fonts.AddFont("Font Awesome 6 Free-Solid-900.otf", "FASolid");
             });
 
         // Initialise the toolkit
