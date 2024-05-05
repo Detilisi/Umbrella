062198515a710bccc6f620558383d73f70149dbf
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-05-05 14:15:46 +0200

    Create UmbrellaSqliteConfigs.cs

---
+++ src/External/Persistance/Common/Configs/UmbrellaSqliteConfigs.cs
@@ -0,0 +1,7 @@
+﻿namespace Persistence.Common.Configs;
+
+internal class UmbrellaSqliteConfigs
+{
+    public static string Database => "umbrella.db3";
+    public static string DatabasePath => Path.Combine(FileSystem.AppDataDirectory, Database);
+}
