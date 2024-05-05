28711866176d01851a27364083c12188f2fc4195
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-05-05 14:31:59 +0200

    Create EmailSyncViewModel.cs

---
+++ src/UI/MauiClientApp/Email/EmailSync/ViewModels/EmailSyncViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Application.Email.Features.Commands.SyncInbox;
+
+namespace MauiClientApp.Email.EmailSync.ViewModels;
+
+public class EmailSyncViewModel(IMediator mediator) : EmailViewModel(mediator, default)
+{
+    //Life cycle 
+    public override async void OnViewModelStarting(CancellationToken token = default)
+    {
+        base.OnViewModelStarting(token);
+
+        //Synx emails
+        await SyncEmailInboxAsync(token);
+    }
+
+    //Sync methods
+    private async Task SyncEmailInboxAsync(CancellationToken token)
+    {
+        var syncCommand = new SyncInboxCommand()
+        {
+            EmailAddress = "",
+            EmailPassword = "",
+        };
+
+        var syncResult = await _mediator.Send(syncCommand, token);
+        if (syncResult.IsFailure) return;
+    }
+}
