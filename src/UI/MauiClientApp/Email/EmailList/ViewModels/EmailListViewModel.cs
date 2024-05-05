d7943387739a0425e47a9e5bac289e58f0fc9234
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-05-05 14:32:04 +0200

    Update EmailListViewModel.cs

--- src/UI/MauiClientApp/Email/EmailList/ViewModels/EmailListViewModel.cs
+++ src/UI/MauiClientApp/Email/EmailList/ViewModels/EmailListViewModel.cs
@@ -1,17 +1,13 @@
-﻿using Application.Email.Features.Commands.SyncInbox;
-using Application.Email.Features.Queries.GetEmailList;
+﻿using Application.Email.Features.Queries.GetEmailList;
 
 namespace MauiClientApp.Email.EmailList.ViewModels;
 
-public class EmailListViewModel(IMediator mediator) : EmailViewModel(default)
+public class EmailListViewModel(IMediator mediator) : EmailViewModel(mediator, default)
 {
-    //Fields
-    private readonly IMediator _mediator = mediator;
-
     //Properties
     public ObservableCollection<EmailModel> EmailMessageList { get; set; } = [];
 
-    //Life cycle
+    //Life cycle 
     public override async void OnViewModelStarting(CancellationToken token = default)
     {
         base.OnViewModelStarting(token);
@@ -23,8 +19,6 @@ public class EmailListViewModel(IMediator mediator) : EmailViewModel(default)
     //Load methods
     private async Task LoadEmailsAsync(CancellationToken token)
     {
-        await SyncEmailInboxAsync(token);
-
         var userId = 1;
         var loadEmailQuery = new GetEmailListQuery(userId);
         var emailList = await _mediator.Send(loadEmailQuery, token);
@@ -34,18 +28,6 @@ public class EmailListViewModel(IMediator mediator) : EmailViewModel(default)
         {
             EmailMessageList.Add(emailModel);
         }
-    }
-
-    private async Task SyncEmailInboxAsync(CancellationToken token)
-    {
-        var syncCommand = new SyncInboxCommand() 
-        {
-            EmailAddress = "",
-            EmailPassword = "",
-        };
-
-        var syncResult = await _mediator.Send(syncCommand, token);
-        if (syncResult.IsFailure) return;
     }
 }
 
