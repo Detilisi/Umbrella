877e00a99a9d97da3f58abd9366dcbce6911639c
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-05-07 20:46:59 +0200

    Create EmailSyncIndicatorView.cs

---
+++ src/UI/MauiClientApp/Email/EmailSync/Views/EmailSyncIndicatorView.cs
@@ -0,0 +1,68 @@
+﻿using SkiaSharp.Extended.UI.Controls;
+
+namespace MauiClientApp.Email.EmailSync.Views;
+
+public class EmailSyncIndicatorView : ContentView
+{
+    //View components
+    private Label HeaderLabel = null!;
+    private Label WaiterLabel = null!;
+    private SKLottieView SyncLottieView = null!;
+
+    //Construction
+    public EmailSyncIndicatorView()
+    {
+        InitializeLabels();
+        InitializeLottieView();
+
+        Content = new VerticalStackLayout()
+        {
+            Spacing = 25,
+            Padding = 30,
+            VerticalOptions = LayoutOptions.Center,
+            Children =
+            {
+                SyncLottieView,
+                HeaderLabel, 
+                WaiterLabel,
+            }
+        };
+    }
+
+    //View component Initialization
+    private void InitializeLabels()
+    {
+        HeaderLabel = new Label()
+        {
+            FontSize = 22,
+            Text = "Syncing emails",
+            FontAttributes = FontAttributes.Bold,
+            HorizontalTextAlignment = TextAlignment.Center,
+        };
+
+        WaiterLabel = new Label()
+        {
+            MaxLines = 1,
+            FontSize = 18,
+            Text = "Please wait...",
+            FontAttributes = FontAttributes.Bold,
+            HorizontalTextAlignment = TextAlignment.Center,
+        };
+    }
+
+    private void InitializeLottieView()
+    {
+        SyncLottieView = new SKLottieView()
+        {
+            RepeatCount = -1,
+            WidthRequest = 200,
+            HeightRequest = 200,
+            BackgroundColor = Colors.White,
+            Source = new SKFileLottieImageSource()
+            {
+                File = "AnikiHamster.json"
+            }
+        };
+    }
+
+}
