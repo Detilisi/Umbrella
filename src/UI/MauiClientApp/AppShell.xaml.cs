63d6c55fa3773a26b41fc7432b667990cde4ad88
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-05-07 21:20:34 +0200

    Update AppShell.xaml.cs

--- src/UI/MauiClientApp/AppShell.xaml.cs
+++ src/UI/MauiClientApp/AppShell.xaml.cs
@@ -1,10 +1,19 @@
-﻿namespace MauiClientApp
+﻿using MauiClientApp.Email.EmailList.Pages;
+
+namespace MauiClientApp
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+
+            RegisterRoutes();
+        }
+
+        private void RegisterRoutes()
+        {
+            Routing.RegisterRoute(nameof(EmailListPage), typeof(EmailListPage));
         }
     }
 }
