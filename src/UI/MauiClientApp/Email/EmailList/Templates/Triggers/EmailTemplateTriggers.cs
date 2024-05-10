3754d26938ad4a609a87dcf75e4a094390bd304b
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-05-10 21:55:15 +0200

    Create EmailTemplateTriggers.cs

---
+++ src/UI/MauiClientApp/Email/EmailList/Templates/Triggers/EmailTemplateTriggers.cs
@@ -0,0 +1,40 @@
+﻿using MauiClientApp.Email.EmailList.Templates.Converters;
+
+namespace MauiClientApp.Email.EmailList.Templates.Triggers;
+
+internal static class EmailTemplateTriggers
+{
+    public static DataTrigger TodayTrigger = new(typeof(Label))
+    {
+        Value = true,
+        Binding = new Binding(nameof(EmailModel.CreatedAt))
+        {
+            Converter = new DateTimeToTodayCheckerConverter()
+        },
+        Setters =
+        {
+            new Setter
+            {
+                Property = Label.TextProperty,
+                Value = new Binding(nameof(EmailModel.CreatedAt), stringFormat: "{0:h:mm tt}")
+            }
+        }
+    };
+
+    public static DataTrigger NotTodayTrigger = new(typeof(Label))
+    {
+        Value = false,
+        Binding = new Binding(nameof(EmailModel.CreatedAt))
+        {
+            Converter = new DateTimeToTodayCheckerConverter()
+        },
+        Setters =
+        {
+            new Setter
+            {
+                Property = Label.TextProperty,
+                Value = new Binding(nameof(EmailModel.CreatedAt), stringFormat: "{0:dd MMM}")
+            }
+        }
+    };
+}
