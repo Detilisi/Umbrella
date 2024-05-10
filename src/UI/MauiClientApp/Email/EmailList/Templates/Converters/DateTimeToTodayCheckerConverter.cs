dd51caf4121a6476a45df432c2c3508a3ba89236
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-05-10 21:54:47 +0200

    Create DateTimeToTodayCheckerConverter.cs

---
+++ src/UI/MauiClientApp/Email/EmailList/Templates/Converters/DateTimeToTodayCheckerConverter.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+namespace MauiClientApp.Email.EmailList.Templates.Converters;
+
+internal class DateTimeToTodayCheckerConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value is DateTime date && date.Date == DateTime.Today;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
