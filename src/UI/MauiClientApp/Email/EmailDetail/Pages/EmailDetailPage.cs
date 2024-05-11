b2ef8580c3ca10fe00b5a6f66491ba97d02fde05
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-05-11 22:06:29 +0200

    Create EmailDetailPage.cs

---
+++ src/UI/MauiClientApp/Email/EmailDetail/Pages/EmailDetailPage.cs
@@ -0,0 +1,35 @@
+﻿using MauiClientApp.Email.EmailDetail.ViewModels;
+
+namespace MauiClientApp.Email.EmailDetail.Pages;
+
+public class EmailDetailPage(EmailDetailViewModel viewModel) : EmailPage<EmailDetailViewModel>(viewModel, new VerticalStackLayout())
+{
+    //View components
+    private static Label SubjectLabel = null!;
+    private static Label BodyTextLabel = null!;
+    //private EmailControlView? EmailControls;
+    private static BoxView SeparatorBoxView = null!;
+
+    protected override ScrollView PageContent => throw new NotImplementedException();
+
+    //View component Initialization
+    private void InitializeViewComponents()
+    {
+        SubjectLabel = new()
+        {
+            Text = BindingContext.CurrentEmail.Subject
+        };
+        BodyTextLabel = new()
+        {
+            Text = BindingContext.CurrentEmail.Body
+        };
+        SeparatorBoxView = new()
+        {
+            Margin = new Thickness(10, 0, 10, 0)
+        };
+
+        SubjectLabel.DynamicResource(View.StyleProperty, "EmailDetailPageSubjectLabel");
+        BodyTextLabel.DynamicResource(View.StyleProperty, "EmailDetailPageBodyTextLabel");
+        SeparatorBoxView.DynamicResource(View.StyleProperty, "EmailDataTemplateSeparator");
+    }
+}
