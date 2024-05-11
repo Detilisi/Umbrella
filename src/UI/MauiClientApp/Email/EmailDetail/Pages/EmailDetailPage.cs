d518e183f218dd2b397ad44d4480b581948789fb
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-05-11 23:12:04 +0200

    Update EmailDetailPage.cs

--- src/UI/MauiClientApp/Email/EmailDetail/Pages/EmailDetailPage.cs
+++ src/UI/MauiClientApp/Email/EmailDetail/Pages/EmailDetailPage.cs
@@ -1,4 +1,5 @@
 ﻿using MauiClientApp.Email.EmailDetail.ViewModels;
+using MauiClientApp.Email.EmailDetail.Views;
 
 namespace MauiClientApp.Email.EmailDetail.Pages;
 
@@ -7,16 +8,29 @@ public class EmailDetailPage: EmailPage<EmailDetailViewModel>
     //View components
     private static Label SubjectLabel = null!;
     private static Label BodyTextLabel = null!;
-    //private EmailControlView? EmailControls;
+    private EmailSenderView EmailSenderView = null!;
     private static BoxView SeparatorBoxView = null!;
 
     //Construction
     public EmailDetailPage(EmailDetailViewModel viewModel) : base(viewModel, new VerticalStackLayout())
     {
+        var currentEmail = BindingContext.CurrentEmail;
+        EmailSenderView = new(currentEmail.SenderName, currentEmail.CreatedAt);
+
         InitializeViewComponents();
     }
 
-    protected override ScrollView PageContent => throw new NotImplementedException();
+    protected override ScrollView PageContent => new()
+    {
+        Padding = 10,
+        Content = new VerticalStackLayout()
+        {
+            SubjectLabel,
+            EmailSenderView,
+            SeparatorBoxView,
+            BodyTextLabel
+        }
+    };
 
     //View component Initialization
     private void InitializeViewComponents()
