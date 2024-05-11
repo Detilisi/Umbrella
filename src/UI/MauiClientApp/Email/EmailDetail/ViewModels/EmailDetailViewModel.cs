fff2f3fe23c9fd3cfc66fd88a5076a563311596b
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-05-11 20:22:24 +0200

    Create EmailDetailViewModel.cs

---
+++ src/UI/MauiClientApp/Email/EmailDetail/ViewModels/EmailDetailViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Application.Email.Features.Queries.GetEmailById;
+
+namespace MauiClientApp.Email.EmailDetail.ViewModels;
+
+public class EmailDetailViewModel(IMediator mediator) : EmailViewModel(mediator, default)
+{
+    //Properties
+    public EmailModel CurrentEmail { get; set; } = null!;
+
+    //Life cycle 
+    public override async void OnViewModelStarting(CancellationToken token = default)
+    {
+        base.OnViewModelStarting(token);
+
+        await LoadCurrentEmailAsync(token);
+    }
+
+    //Load methods
+    private async Task LoadCurrentEmailAsync(CancellationToken token)
+    {
+        var userId = 1;
+        var loadEmailQuery = new GetEmailByIdQuery(userId, 1);
+        var emailMessage = await _mediator.Send(loadEmailQuery, token);
+        if (emailMessage.IsFailure) return;
+
+        CurrentEmail = emailMessage.Value;
+    }
+}
