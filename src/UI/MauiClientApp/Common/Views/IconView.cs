0d301d7683863933d02210a1fc15a741de64fd9c
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-05-12 21:14:45 +0200

    Create IconView.cs

---
+++ src/UI/MauiClientApp/Common/Views/IconView.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiClientApp.Common.Controls;
+
+public class IconView : Label
+{
+    //Construction
+    public IconView(string fontIcon)
+    {
+        Text = fontIcon;
+        this.DynamicResource(View.StyleProperty, "IconLabelStyle");
+    }
+}
