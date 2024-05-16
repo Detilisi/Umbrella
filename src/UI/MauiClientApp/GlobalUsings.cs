248c8a729fbe8b76a4fd2e8867c71773326c1bff
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-05-16 22:47:29 +0200

    Update GlobalUsings.cs

--- src/UI/MauiClientApp/GlobalUsings.cs
+++ src/UI/MauiClientApp/GlobalUsings.cs
@@ -4,6 +4,8 @@ global using CommunityToolkit.Maui.Markup;
 global using System.Collections.ObjectModel;
 global using CommunityToolkit.Mvvm.ComponentModel;
 
+global using Shared.Common.Results;
+
 global using Application.Common.Base;
 global using Application.Email.Base;
 global using Application.User.Models;
@@ -15,3 +17,10 @@ global using MauiClientApp.Common.Base;
 global using MauiClientApp.Common.Views;
 global using Umbrella.Maui.Email.Base.Pages;
 global using MauiClientApp.Resources.Fonts.Icons;
+
+global using MauiClientApp.Email.EmailDetail.Pages;
+global using MauiClientApp.Email.EmailDetail.ViewModels;
+global using MauiClientApp.Email.EmailList.Pages;
+global using MauiClientApp.Email.EmailList.ViewModels;
+global using MauiClientApp.Email.EmailSync.Pages;
+global using MauiClientApp.Email.EmailSync.ViewModels;
