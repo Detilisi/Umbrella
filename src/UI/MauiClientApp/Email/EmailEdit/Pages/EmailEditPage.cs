6aa81336b717af7b18b273380b47be61a44a6bde
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-05-17 21:17:57 +0200

    Create EmailEditPage.cs

---
+++ src/UI/MauiClientApp/Email/EmailEdit/Pages/EmailEditPage.cs
@@ -0,0 +1,12 @@
+﻿using MauiClientApp.Email.EmailEdit.ViewModels;
+
+namespace MauiClientApp.Email.EmailEdit.Pages;
+
+internal class EmailEditPage : EmailPage<EmailEditViewModell>
+{
+    public EmailEditPage(EmailEditViewModell viewModel) : base(viewModel)
+    {
+    }
+
+    protected override ScrollView PageContent => throw new NotImplementedException();
+}
