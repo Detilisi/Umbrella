16305151a0bf246be7c798a6ff9e5d6329248d9f
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-05-17 21:54:03 +0200

    Update EmailEditViewModel.cs

--- src/UI/MauiClientApp/Email/EmailEdit/ViewModels/EmailEditViewModel.cs
+++ src/UI/MauiClientApp/Email/EmailEdit/ViewModels/EmailEditViewModel.cs
@@ -4,4 +4,21 @@ internal partial class EmailEditViewModell(IMediator mediator) : EmailViewModel(
 {
     //Properties
     public EmailModel CurrentEditEmail { get; set; } = null!;
+
+    //Life cycle 
+    public override void OnViewModelStarting(CancellationToken token = default)
+    {
+        base.OnViewModelStarting(token);
+
+        //load emails
+        CurrentEditEmail = new() 
+        {
+            Sender = "",
+            SenderName = "",
+            Recipients = [],
+            Subject = "",
+            Body = "",
+            EmailStatus = Domain.Email.Entities.Enums.EmailStatus.Draft
+        };
+    }
 }
