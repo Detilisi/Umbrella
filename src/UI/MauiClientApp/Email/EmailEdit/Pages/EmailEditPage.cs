23e292fccb4e7f899392272952c344bf6ef7effe
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-05-17 22:25:43 +0200

    Update EmailEditPage.cs

--- src/UI/MauiClientApp/Email/EmailEdit/Pages/EmailEditPage.cs
+++ src/UI/MauiClientApp/Email/EmailEdit/Pages/EmailEditPage.cs
@@ -2,11 +2,15 @@
 
 namespace MauiClientApp.Email.EmailEdit.Pages;
 
-internal class EmailEditPage : EmailPage<EmailEditViewModell>
+internal class EmailEditPage(EmailEditViewModell viewModel) : EmailPage<EmailEditViewModell>(viewModel)
 {
-    public EmailEditPage(EmailEditViewModell viewModel) : base(viewModel)
-    {
-    }
+    //View components
+    private static Grid ContentGrid = null!;
+    private static Label SenderEmailLabel = null!;
+    private static Label RecipientsEmailsLabel = null!;
+
+    private static Label SubjectLineLabel = null!;
+    private static Editor BodyTextEditor = null!;
 
     protected override ScrollView PageContent => throw new NotImplementedException();
 }
