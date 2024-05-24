a52b1b933f3c2d93d6c04c10f3cc85f5eb5c16f3
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-05-24 20:41:17 +0200

    Update EmailEditViewModel.cs

--- src/External/Infrastructure/DependencyInjection.cs
+++ src/External/Infrastructure/DependencyInjection.cs
@@ -6,6 +6,7 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructureLayer(this IServiceCollection services)
     {
+        services.AddSingleton<IEmailSender, EmailSender>();
         services.AddSingleton<IEmailFetcher, EmailFetcher>();
 
         return services;
