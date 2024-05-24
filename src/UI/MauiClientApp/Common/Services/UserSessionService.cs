3a06c78ac81850074e78d25f66bc2eebe8632531
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-05-24 19:37:39 +0200

    Create UserSessionService.cs

---
+++ src/UI/MauiClientApp/Common/Services/UserSessionService.cs
@@ -0,0 +1,7 @@
+﻿namespace MauiClientApp.Common.Services;
+
+internal static class UserSessionService
+{
+    public static bool IsAuthenticated { get; set; } = false;
+    public static UserModel CurrentUser { set; get; } = null!;
+}
