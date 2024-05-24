ab3101263b846bc4e3de20fd8a406d0124c56afe
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-05-24 18:50:14 +0200

    Update EmailEditViewModel.cs

--- src/UI/MauiClientApp/Email/EmailEdit/ViewModels/EmailEditViewModel.cs
+++ src/UI/MauiClientApp/Email/EmailEdit/ViewModels/EmailEditViewModel.cs
@@ -2,16 +2,29 @@
 
 public partial class EmailEditViewModell(IMediator mediator) : EmailViewModel(mediator, default)
 {
+    //Properties
+    [ObservableProperty]
+    public EmailModel emailDraft = null!;
+
     //Life cycle 
     public override void OnViewModelStarting(CancellationToken token = default)
     {
         base.OnViewModelStarting(token);
+
+        EmailDraft = new() 
+        {
+            Sender = "sender@gmail.com",
+            SenderName = string.Empty,
+            Subject = string.Empty,
+            Body = string.Empty,
+        };
     }
 
     //Commands
     [RelayCommand]
-    public static async Task SendEmail(EmailModel emailModel)
+    public async Task SendEmail()
     {
+        var test = EmailDraft;
         await NavigationService.NavigateToViewModelAsync<EmailListViewModel>();
     }
 }
