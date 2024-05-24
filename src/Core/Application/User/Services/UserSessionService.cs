a8b89f57d0f674614ac1618d1cdb12b4e9679de8
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-05-24 19:25:42 +0200

    Create UserSessionService.cs

---
+++ src/Core/Application/User/Services/UserSessionService.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.User.Services;
+
+public static class UserSessionService
+{
+    public static bool IsAuthenticated { get; set; } = false;
+    public static UserModel CurrentUser { set; get; } = null!;
+}
