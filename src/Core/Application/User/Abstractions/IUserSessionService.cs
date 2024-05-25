b0f15272e835ecaaa82d15c1e170da1d01ae8d96
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-05-25 16:05:52 +0200

    Add new UserSessionService

---
+++ src/Core/Application/User/Abstractions/IUserSessionService.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.User.Abstractions;
+
+public interface IUserSessionService
+{
+    public Result CreateSession(UserModel user, CancellationToken token = default);
+    public Result<UserModel> GetCurrentSession(CancellationToken token = default);
+    public Result DeleteCurrentSession(CancellationToken token = default);
+}
---
+++ src/Core/Application/User/Services/UserSessionService.cs
@@ -0,0 +1,41 @@
+﻿using Application.User.Abstractions;
+using Application.User.Errors;
+
+namespace Application.User.Services;
+
+public class UserSessionService : IUserSessionService
+{
+    private static bool _isAuthenticated;
+    private static UserModel? _currentUser;
+
+    public Result CreateSession(UserModel currentUser, CancellationToken token = default)
+    {
+        if (_isAuthenticated) return Result.Success();
+
+        _isAuthenticated = true;
+        _currentUser = currentUser;
+
+        return Result.Success();
+    }
+
+    public Result DeleteCurrentSession(CancellationToken token = default)
+    {
+        if (!_isAuthenticated) return Result.Success();
+
+        _isAuthenticated = false;
+        _currentUser = null;
+
+        return Result.Success();
+    }
+
+    public Result<UserModel> GetCurrentSession(CancellationToken token = default)
+    {
+        if (!_isAuthenticated || _currentUser is null)
+        {
+            return Result.Failure<UserModel>(AuthenticationErrors.SessionNotAuthenticated);
+        }
+
+        return Result.Success(_currentUser);
+    }
+}
+
