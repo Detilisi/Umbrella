2cf0f8bf4015e64362eebc55152cb10b9a4351c1
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-05-29 21:04:05 +0200

    Update EmailDetailViewModel.cs

--- src/UI/MauiClientApp/Email/EmailDetail/ViewModels/EmailDetailViewModel.cs
+++ src/UI/MauiClientApp/Email/EmailDetail/ViewModels/EmailDetailViewModel.cs
@@ -1,22 +1,9 @@
-﻿using Application.Email.Features.Queries.GetEmailById;
-
-namespace MauiClientApp.Email.EmailDetail.ViewModels;
+﻿namespace MauiClientApp.Email.EmailDetail.ViewModels;
 
 public partial class EmailDetailViewModel(IMediator mediator) : EmailViewModel(mediator, default), IQueryAttributable
 {
     //Properties
     public EmailModel CurrentEmail { get; set; } = null!;
-
-    //Load methods
-    private async Task LoadCurrentEmailAsync(CancellationToken token)
-    {
-        var userId = 1;
-        var loadEmailQuery = new GetEmailByIdQuery(userId, 1);
-        var emailMessage = await _mediator.Send(loadEmailQuery, token);
-        if (emailMessage.IsFailure) return;
-
-        CurrentEmail = emailMessage.Value;
-    }
 
     //Navigation
     public void ApplyQueryAttributes(IDictionary<string, object> query)
