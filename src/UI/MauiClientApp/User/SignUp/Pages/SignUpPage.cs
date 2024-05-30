2e921b146e7fcdefd5cb812c2af03d7b2b86b35d
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-05-30 20:27:01 +0200

    Create SignUpPage.cs

---
+++ src/UI/MauiClientApp/User/SignUp/Pages/SignUpPage.cs
@@ -0,0 +1,18 @@
+﻿using MauiClientApp.User.SignUp.Views;
+
+namespace MauiClientApp.User.SignUp.Pages;
+
+internal class SignUpPage : Page<SignUpViewModel>
+{
+    //Construction
+    public SignUpPage(SignUpViewModel viewModel) : base(viewModel)
+    {
+        Content = new VerticalStackLayout()
+        {
+            Children =
+            {
+                new SignUpHeaderView()
+            }
+        };
+    }
+}
