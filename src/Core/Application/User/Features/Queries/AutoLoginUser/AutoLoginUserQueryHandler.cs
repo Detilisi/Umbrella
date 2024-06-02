832b4c931e2630f20bfe3f66665bc71c3ab8ccb5
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-06-02 20:20:49 +0200

    Create AutoLoginUserQueryHandler.cs

---
+++ src/Core/Application/User/Features/Queries/AutoLoginUser/AutoLoginUserQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿namespace Application.User.Features.Queries.AutoLoginUser;
+
+public class AutoLoginUserQueryHandler(IApplicationDbContext dbContext, IUserSessionService userSessionService) : IRequestHandler<AutoLoginUserQuery, Result>
+{
+    //Fields
+    private readonly IApplicationDbContext _dbContext = dbContext;
+    private readonly IUserSessionService _userSessionService = userSessionService;
+
+    public async Task<Result> Handle(AutoLoginUserQuery request, CancellationToken cancellationToken)
+    {
+        var userEntity = await _dbContext.Users.FirstOrDefaultAsync(cancellationToken);
+        if (userEntity == null) return Result.Failure(new Error("No registered user", ""));
+
+        var userModel = UserModel.CreateFromEntity(userEntity);
+        _userSessionService.CreateSession(userModel);
+
+        return Result.Success();
+    }
+}
