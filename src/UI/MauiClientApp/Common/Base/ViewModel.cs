a79c6a9552fedc8fadf72afc53be8f93efef5318
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-06-10 19:43:15 +0200

    Update ViewModel.cs

--- src/UI/MauiClientApp/Common/Base/ViewModel.cs
+++ src/UI/MauiClientApp/Common/Base/ViewModel.cs
@@ -1,16 +1,81 @@
-﻿using System.Diagnostics;
+﻿using Application.Common.Abstractions.Services;
+using MauiClientApp.Common.ChatHistory.Emums;
+using MauiClientApp.Common.ChatHistory.Models;
+using System.Diagnostics;
 
 namespace MauiClientApp.Common.Base;
 
 public abstract partial class ViewModel : ObservableObject
 {
+    //Services
+    private readonly IAppTextToSpeech _textToSpeech;
+    private readonly IAppSpeechRecognition _speechRecognition;
+
+    //Fields
+    private static bool MicrophoneUsable { get; set; } = false;
+    [ObservableProperty] public bool isListening = false;
+    private ObservableCollection<ChatHistoryModel> ChatHistory { get; set; } = [];
+
     //ViewModel lifecylce
-    public virtual void OnViewModelStarting(CancellationToken cancellationToken = default)
+    public virtual async void OnViewModelStarting(CancellationToken token = default)
     {
         Debug.WriteLine($"{GetType().Name} is starting");
+
+        if(MicrophoneUsable) return;
+        MicrophoneUsable = await _speechRecognition.RequestPermissions(token);
     }
-    public virtual void OnViewModelClosing(CancellationToken cancellationToken = default)
+    public virtual async void OnViewModelClosing(CancellationToken token = default)
     {
         Debug.WriteLine($"{GetType().Name} is closing");
+
+        await _speechRecognition.StopListenAsync(token);
+    }
+
+    //Text-to-speech methods
+    public async Task SpeakAsync(string messageText)
+    {
+        if (IsListening || string.IsNullOrEmpty(messageText)) return;
+
+        ChatHistory.Add(new() 
+        {
+            Sender = ChatSender.Bot,
+            Message = messageText
+        });
+        await _textToSpeech.SpeakAsync(messageText);
+    }
+
+    public async Task ListenAsync()
+    {
+        if (IsListening) return;
+
+        try
+        {
+            IsListening = true;
+            if (!MicrophoneUsable)
+            {
+                await SpeakAsync("Permission not granted to use microphone.");
+                return;
+            }
+
+            var listenResult = await _speechRecognition.ListenAsync();
+            if (listenResult.IsFailure)
+            {
+                await SpeakAsync(listenResult.Error.Message);
+            }
+
+            ChatHistory.Add(new()
+            {
+                Sender = ChatSender.Bot,
+                Message = listenResult.Value
+            });
+        }
+        catch
+        {
+            await SpeakAsync("Speech recognition has failed!");
+        }
+        finally
+        {
+            IsListening = false;
+        }
     }
 }
