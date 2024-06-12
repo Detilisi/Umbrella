4d400fcfcfa71ec26f45dfb5ff987e7a16e99f25
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-06-12 21:49:58 +0200

    Create SpeechRecognition.cs

---
+++ src/UI/MauiClientApp/Common/Services/SpeechRecognition.cs
@@ -0,0 +1,44 @@
+﻿using CommunityToolkit.Maui.Media;
+using System.Globalization;
+
+namespace MauiClientApp.Common.Services;
+
+internal class SpeechRecognition
+{
+    //Fields
+    const string defaultLanguage = "en-US";
+
+    //Fields
+    private bool _canStartListenExecute;
+    private static ISpeechToText _speechToText = SpeechToText.Default;
+
+    //Permision method
+    public void RequestPermissions(CancellationToken cancellationToken = default)
+    {
+        _speechToText.RequestPermissions(cancellationToken);
+    }
+
+    //Listen methods
+    public async Task StartListen(CancellationToken cancellationToken)
+    {
+        if (!_canStartListenExecute) return;
+        _canStartListenExecute = true;
+
+        await _speechToText.StartListenAsync(CultureInfo.GetCultureInfo(defaultLanguage), cancellationToken);
+        _speechToText.RecognitionResultUpdated += HandleRecognitionResultUpdated;
+    }
+    public async Task StopListen(CancellationToken cancellationToken)
+    {
+        _canStartListenExecute = true;
+
+        _speechToText.RecognitionResultUpdated -= HandleRecognitionResultUpdated;
+
+        await _speechToText.StopListenAsync(cancellationToken);
+    }
+
+    //Helpers
+    private void HandleRecognitionResultUpdated(object? sender, SpeechToTextRecognitionResultUpdatedEventArgs e)
+    {
+        var text = e.RecognitionResult;
+    }
+}
