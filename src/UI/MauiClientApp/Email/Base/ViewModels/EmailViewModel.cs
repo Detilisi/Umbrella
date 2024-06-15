564a458e5566f031a7289dd2faac96e058f4b96c
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-06-16 00:28:06 +0200

    Update EmailViewModel.cs

--- src/UI/MauiClientApp/Email/Base/ViewModels/EmailViewModel.cs
+++ src/UI/MauiClientApp/Email/Base/ViewModels/EmailViewModel.cs
@@ -9,6 +9,18 @@ internal partial class EmailViewModel(IMediator mediator) : ViewModel
     public override void OnViewModelStarting(CancellationToken token = default)
     {
         base.OnViewModelStarting(token);
+
+        if (SpeechService.OnSpeechAnounced == null || SpeechService.OnSpeechRecognized == null) return;
+        SpeechService.OnSpeechAnounced = text => ChatHistory.Add(new ChatHistoryModel()
+        {
+            Sender = ChatSender.Bot,
+            Message = text
+        });
+        SpeechService.OnSpeechRecognized = recognitionText => ChatHistory.Add(new ChatHistoryModel()
+        {
+            Sender = ChatSender.Human,
+            Message = recognitionText
+        });
     }
 
     public override void OnViewModelClosing(CancellationToken cancellationToken = default)
