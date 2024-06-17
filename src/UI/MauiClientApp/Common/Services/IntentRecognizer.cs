cf45fdc58a34321b8f7e2059b68bd3932811eac2
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-06-17 17:45:03 +0200

    Create IntentRecognizer.cs

---
+++ src/UI/MauiClientApp/Common/Services/IntentRecognizer.cs
@@ -0,0 +1,12 @@
+﻿using MauiClientApp.Common.Enums;
+
+namespace MauiClientApp.Common.Services;
+
+internal class IntentRecognizer
+{
+    internal static SpeechCommands GetIntent(string text) 
+    {
+        
+        return SpeechCommands.CancelOperation;
+    }
+}
