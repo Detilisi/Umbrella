3bc6b2911bbbd566397064692bf79a955efbc0a7
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-06-18 20:47:42 +0200

    Update IntentRecognizer.cs

--- src/UI/MauiClientApp/Common/Services/IntentRecognizer.cs
+++ src/UI/MauiClientApp/Common/Services/IntentRecognizer.cs
@@ -3,64 +3,43 @@ using static System.Net.Mime.MediaTypeNames;
 
 namespace MauiClientApp.Common.Services;
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 internal class IntentRecognizer
 {
-    internal static UserIntent GetIntent(string text) 
+    private static readonly Dictionary<UserIntent, HashSet<string>> IntentCommands = new Dictionary<UserIntent, HashSet<string>>
     {
-        string evalString = text.ToLower();
-        if (string.IsNullOrEmpty(text)) return UserIntent.Undefined;
-
-        var writeCommands = new List<string>
-        {
-            "Compose",
-            "Draft",
-            "Write",
-            "Send",
-            "Type",
-            "Create",
-            "Prepare",
-            "Pen down",
-            "Construct",
-            "Craft",
-            "Formulate",
-            "Produce",
-            "Develop",
-            "Email",
-            "Message",
-            "Shoot off",
-            "Scribble",
-            "Jot down",
-            "Fire off"
-        };
-        if (writeCommands.Any(evalString.Contains))
-        {
-            return UserIntent.WriteEmail;
+        { UserIntent.WriteEmail, new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+            {
+                "Compose", "Draft", "Write", "Send", "Type", "Create", "Prepare", "Pen down", "Construct",
+                "Craft", "Formulate", "Produce", "Develop", "Email", "Message", "Shoot off", "Scribble",
+                "Jot down", "Fire off"
+            }
+        },
+        { UserIntent.ReadEmails, new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+            {
+                "Read", "Check", "Review", "Scan", "Skim", "Look over", "Browse", "Peruse", "Inspect",
+                "Examine", "Glance at", "Go through", "Scroll through", "Study", "Monitor", "Look through",
+                "Analyze", "Verify", "Observe", "Take a look"
+            }
         }
+    };
 
-        var readEmailCommands = new List<string>
+    internal static UserIntent GetIntent(string text)
+    {
+        if (string.IsNullOrWhiteSpace(text)) return UserIntent.Undefined;
+
+        string evalString = text.ToLower();
+
+        foreach (var intent in IntentCommands)
         {
-            "Read",
-            "Check",
-            "Review",
-            "Scan",
-            "Skim",
-            "Look over",
-            "Browse",
-            "Peruse",
-            "Inspect",
-            "Examine",
-            "Glance at",
-            "Go through",
-            "Scroll through",
-            "Study",
-            "Monitor",
-            "Look through",
-            "Analyze",
-            "Verify",
-            "Observe",
-            "Take a look"
-        };
-
+            if (intent.Value.Any(command => evalString.Contains(command.ToLower())))
+            {
+                return intent.Key;
+            }
+        }
 
         return UserIntent.CancelOperation;
     }
