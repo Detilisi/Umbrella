8fbeb94d734ef66847ccc99f9c9c696e82910b61
Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-06-18 18:57:50 +0200

    Update IntentRecognizer.cs

--- src/UI/MauiClientApp/Common/Services/IntentRecognizer.cs
+++ src/UI/MauiClientApp/Common/Services/IntentRecognizer.cs
@@ -1,12 +1,67 @@
 ﻿using MauiClientApp.Common.Enums;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace MauiClientApp.Common.Services;
 
 internal class IntentRecognizer
 {
-    internal static SpeechCommands GetIntent(string text) 
+    internal static UserIntent GetIntent(string text) 
     {
-        
-        return SpeechCommands.CancelOperation;
+        string evalString = text.ToLower();
+        if (string.IsNullOrEmpty(text)) return UserIntent.Undefined;
+
+        var writeCommands = new List<string>
+        {
+            "Compose",
+            "Draft",
+            "Write",
+            "Send",
+            "Type",
+            "Create",
+            "Prepare",
+            "Pen down",
+            "Construct",
+            "Craft",
+            "Formulate",
+            "Produce",
+            "Develop",
+            "Email",
+            "Message",
+            "Shoot off",
+            "Scribble",
+            "Jot down",
+            "Fire off"
+        };
+        if (writeCommands.Any(evalString.Contains))
+        {
+            return UserIntent.WriteEmail;
+        }
+
+        var readEmailActionWords = new List<string>
+        {
+            "Read",
+            "Check",
+            "Review",
+            "Scan",
+            "Skim",
+            "Look over",
+            "Browse",
+            "Peruse",
+            "Inspect",
+            "Examine",
+            "Glance at",
+            "Go through",
+            "Scroll through",
+            "Study",
+            "Monitor",
+            "Look through",
+            "Analyze",
+            "Verify",
+            "Observe",
+            "Take a look"
+        };
+
+
+        return UserIntent.CancelOperation;
     }
 }
