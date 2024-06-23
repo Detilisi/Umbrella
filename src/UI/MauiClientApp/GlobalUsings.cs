11b4174da7e5ff6b442ec9b52e068728b4c4beb3
Bongani Elijah Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-06-23 21:05:30 +0200

    Update GlobalUsings.cs

--- src/UI/MauiClientApp/GlobalUsings.cs
+++ src/UI/MauiClientApp/GlobalUsings.cs
@@ -1,36 +1,29 @@
-﻿global using MediatR;
-global using CommunityToolkit.Maui;
-global using CommunityToolkit.Mvvm.Input;
-global using CommunityToolkit.Maui.Markup;
-global using System.Collections.ObjectModel;
-global using CommunityToolkit.Mvvm.ComponentModel;
-
-global using Shared.Common.Results;
-
-global using Application.Common.Base;
-global using Application.Email.Base;
-global using Application.User.Models;
+﻿global using Application.Email.Base;
 global using Application.Email.Models;
-global using Application.Email.Abstractions.Services;
-global using Application.Common.Abstractions.DataContexts;
-
+global using CommunityToolkit.Maui;
+global using CommunityToolkit.Maui.Markup;
+global using CommunityToolkit.Mvvm.ComponentModel;
+global using CommunityToolkit.Mvvm.Input;
 global using MauiClientApp.Common.Base;
-global using MauiClientApp.Common.Services;
-global using MauiClientApp.Common.Views;
 global using MauiClientApp.Common.ChatHistory.Emums;
 global using MauiClientApp.Common.ChatHistory.Models;
-
-global using Umbrella.Maui.Email.Base.Pages;
-global using MauiClientApp.Resources.Fonts.Icons;
-
+global using MauiClientApp.Common.Enums;
+global using MauiClientApp.Common.Services;
+global using MauiClientApp.Common.Views;
 global using MauiClientApp.Email.EmailDetail.Pages;
 global using MauiClientApp.Email.EmailDetail.ViewModels;
+global using MauiClientApp.Email.EmailEdit.Pages;
+global using MauiClientApp.Email.EmailEdit.ViewModels;
 global using MauiClientApp.Email.EmailList.Pages;
 global using MauiClientApp.Email.EmailList.ViewModels;
 global using MauiClientApp.Email.EmailSync.Pages;
 global using MauiClientApp.Email.EmailSync.ViewModels;
-global using MauiClientApp.Email.EmailEdit.Pages;
-global using MauiClientApp.Email.EmailEdit.ViewModels;
+global using MauiClientApp.Resources.Fonts.Icons;
+global using MauiClientApp.Resources.Strings;
 global using MauiClientApp.User.SignUp.Pages;
 global using MauiClientApp.User.SignUp.ViewModels;
+global using MediatR;
+global using Shared.Common.Results;
+global using System.Collections.ObjectModel;
+global using Umbrella.Maui.Email.Base.Pages;
 
