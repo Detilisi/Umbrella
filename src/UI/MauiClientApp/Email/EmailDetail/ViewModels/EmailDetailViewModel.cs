6854a73bf23368a7bf378f0b90ffe75afdff172f
Bongani Elijah Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-06-24 20:16:54 +0200

    Update EmailDetailViewModel.cs

--- src/UI/MauiClientApp/Email/EmailDetail/ViewModels/EmailDetailViewModel.cs
+++ src/UI/MauiClientApp/Email/EmailDetail/ViewModels/EmailDetailViewModel.cs
@@ -11,4 +11,24 @@ internal partial class EmailDetailViewModel(IMediator mediator) : EmailViewModel
         var selectedEmail = (EmailModel)query[nameof(EmailModel)];
         CurrentEmail = selectedEmail;
     }
+
+    //VM conversationm
+    private async Task StartVMConversationm()
+    {
+        //Introduction
+        await SpeechService.SpeakAsync("Welcome to the email reading page");
+        await SpeechService.SpeakAsync($"This message was sent by {CurrentEmail.SenderName} at {CurrentEmail.CreatedAt}");
+        await SpeechService.SpeakAsync($"Email subject line: {CurrentEmail.Subject}");
+
+        //Get user inpit 
+        await SpeechService.SpeakAsync("Should I proceed reading this message?");
+        var userIntent = await ListenAndUserIntent();
+        if (userIntent == UserIntent.Affirm)
+        {
+            await SpeechService.SpeakAsync($"Sure thing, the email body reads: {CurrentEmail.Body}");
+            await SpeechService.SpeakAsync("Do you want me to read it again?");
+
+            var userIntent1 = await ListenAndUserIntent();
+        }
+    }
 }
