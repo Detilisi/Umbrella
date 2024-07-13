15f8b55cf0903611f6df18e41198e5ebf0aa3704
Bongani Elijah Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-07-13 23:06:04 +0200

    Update ViewModel.cs

--- src/UI/MauiClientApp/Common/Base/ViewModel.cs
+++ src/UI/MauiClientApp/Common/Base/ViewModel.cs
@@ -6,16 +6,20 @@ internal abstract partial class ViewModel : ObservableObject
 {
     internal static ObservableCollection<ChatHistoryModel> ChatHistory { get; private set; } = [];
 
-    public virtual void OnViewModelStarting(CancellationToken token = default)
-    {
-        Debug.WriteLine($"{GetType().Name} is closing");
-    }
-    public virtual void OnViewModelClosing(CancellationToken token = default)
+    [RelayCommand]
+    public virtual void OnViewModelStarting()
     {
         Debug.WriteLine($"{GetType().Name} is closing");
     }
 
-    public virtual void OnViewModelHasFocus(CancellationToken token = default)
+    [RelayCommand]
+    public virtual void OnViewModelClosing()
+    {
+        Debug.WriteLine($"{GetType().Name} is closing");
+    }
+
+    [RelayCommand]
+    public virtual void OnViewModelHasFocus()
     {
         Debug.WriteLine($"{GetType().Name} has focus at {DateTime.Now}");
     }
