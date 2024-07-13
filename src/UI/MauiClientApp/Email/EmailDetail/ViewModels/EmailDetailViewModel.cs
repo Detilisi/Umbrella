6d22bf7f3e57ac7ec37f4a2a3129e3e69233a906
Bongani Elijah Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-07-13 22:29:17 +0200

    Update EmailDetailViewModel.cs

--- src/UI/MauiClientApp/Email/EmailDetail/ViewModels/EmailDetailViewModel.cs
+++ src/UI/MauiClientApp/Email/EmailDetail/ViewModels/EmailDetailViewModel.cs
@@ -28,27 +28,29 @@ internal partial class EmailDetailViewModel(IMediator mediator) : EmailViewModel
     //Handler methods
     public override async Task HandleUserInteraction()
     {
+        var token = _cancellationTokenSource.Token;
+
         //Introduction
-        await SpeechService.SpeakAsync(UiStrings.ReadingInfo_Introduction);
+        await SpeechService.SpeakAsync(UiStrings.ReadingInfo_Introduction, token);
         await SpeechService.SpeakAsync(string.Format(UiStrings.ReadingInfo_EmailSummary, 
-            CurrentEmail.SenderName, CurrentEmail.CreatedAt));
-        await SpeechService.SpeakAsync(string.Format(UiStrings.ReadingInfo_Subject, CurrentEmail.Subject));
+            CurrentEmail.SenderName, CurrentEmail.CreatedAt), token);
+        await SpeechService.SpeakAsync(string.Format(UiStrings.ReadingInfo_Subject, CurrentEmail.Subject), token);
 
         //Get user inpit 
-        await SpeechService.SpeakAsync(UiStrings.ReadingQuery_ReadEmail);
+        await SpeechService.SpeakAsync(UiStrings.ReadingQuery_ReadEmail, token);
         var userIntent = await ListenAndUserIntent();
         if (userIntent == UserIntent.Yes)
         {
-            read:  await SpeechService.SpeakAsync(UiStrings.ReadingReponse_ReadEmail);
-            var result = Task.Run(async () => await SpeechService.SpeakAsync(CurrentEmail.Body)).Result;
-            await SpeechService.SpeakAsync(UiStrings.ReadingQuery_RepeatRead);
+            read:  await SpeechService.SpeakAsync(UiStrings.ReadingReponse_ReadEmail, token);
+            var result = Task.Run(async () => await SpeechService.SpeakAsync(CurrentEmail.Body, token)).Result;
+            await SpeechService.SpeakAsync(UiStrings.ReadingQuery_RepeatRead, token);
             
             var userIntent1 = await ListenAndUserIntent();
             if(userIntent1 == UserIntent.Yes) goto read;
         }
         else 
         {
-            await SpeechService.SpeakAsync(UiStrings.ReadingQuery_RepeatDelete);
+            await SpeechService.SpeakAsync(UiStrings.ReadingQuery_RepeatDelete, token);
             var userIntent2 = await ListenAndUserIntent();
             if (userIntent2 == UserIntent.ReplyEmail) await ReplyEmailCommand.ExecuteAsync(null);
             else if (userIntent2 == UserIntent.ForwardEmail) await ForwardEmailCommand.ExecuteAsync(null);
