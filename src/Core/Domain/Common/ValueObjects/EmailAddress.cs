a37e684dad3aec22a21543d846d5edfbade8632b
Bongani Elijah Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-07-15 22:20:11 +0200

    Update EmailAddress.cs

--- src/Core/Domain/Common/ValueObjects/EmailAddress.cs
+++ src/Core/Domain/Common/ValueObjects/EmailAddress.cs
@@ -29,7 +29,7 @@ public class EmailAddress : ValueObject<string>
     }
 
     //Helper methods
-    private static bool IsValidEmail(string email)
+    public static bool IsValidEmail(string email)
     {
         string emailPattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
         return Regex.IsMatch(email, emailPattern);
