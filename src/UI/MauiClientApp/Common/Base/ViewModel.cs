29e4554aeb4227792d17209b8e57fbfffcc6e86a
Bongani Elijah Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-07-16 22:23:12 +0200

    Update ViewModel.cs

--- src/UI/MauiClientApp/Common/Base/ViewModel.cs
+++ src/UI/MauiClientApp/Common/Base/ViewModel.cs
@@ -4,6 +4,7 @@ namespace MauiClientApp.Common.Base;
 
 internal abstract partial class ViewModel : ObservableObject
 {
+    [ObservableProperty] internal bool isBusy;
     internal static ObservableCollection<ChatHistoryModel> ChatHistory { get; private set; } = [];
 
     public virtual void OnViewModelStarting()
@@ -19,5 +20,15 @@ internal abstract partial class ViewModel : ObservableObject
     public virtual void OnViewModelHasFocus()
     {
         Debug.WriteLine($"{GetType().Name} has focus at {DateTime.Now}");
+    }
+
+    //Helper methods
+    public void FireViewModelBusy()
+    {
+        IsBusy = true;
+    }
+    public void FireViewModelNotBusy()
+    {
+        IsBusy = false;
     }
 }
