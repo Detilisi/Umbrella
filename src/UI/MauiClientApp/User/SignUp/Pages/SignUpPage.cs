c611ae5c9f9548902ef32f7663d73dc7f069ff34
Bongani Elijah Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-07-17 20:55:12 +0200

    Update SignUpPage.cs

--- src/UI/MauiClientApp/User/SignUp/Pages/SignUpPage.cs
+++ src/UI/MauiClientApp/User/SignUp/Pages/SignUpPage.cs
@@ -31,7 +31,7 @@ internal class SignUpPage : Page<SignUpViewModel>
 
     private void InitializeViewComponents()
     {
-        Title = "Sign up";
+        Title = "Let's get started!";
         EmailEntry = new Entry { Placeholder = "Email:" };
         EmailEntry.DynamicResource(StyleProperty, "SignUpEntry");
         EmailEntry.Bind(Entry.TextProperty, static (SignUpViewModel vm) => vm.UserEmail,
