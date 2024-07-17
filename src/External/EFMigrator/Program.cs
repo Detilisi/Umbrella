6d3d1c42db24ca4a82b38b1903b215ea19008540
Bongani Elijah Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-07-17 20:49:40 +0200

    Update Program.cs

--- src/External/EFMigrator/Program.cs
+++ src/External/EFMigrator/Program.cs
@@ -1,5 +1,5 @@
-﻿// Step 1: Set start up project to Migrator project
-// Step 2: Open PMC and set Persistance as defualt project
+﻿// Step 1: Set start up project to EFMigrator project
+// Step 2: Open Package Manager Console and set Persistance as defualt project
 // Step 3: Run this -> add-migration Initial -Context Persistence.Common.DataContexts.ApplicationDbContext -Verbose
 
 using Persistence.Common.DataContexts;
