92016a420e9691e952c2f62611195fd61cf35df3
Bongani Elijah Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-07-31 21:36:22 +0200

    Update EmailSenderView.cs

--- src/UI/MauiClientApp/Email/EmailDetail/Views/EmailSenderView.cs
+++ src/UI/MauiClientApp/Email/EmailDetail/Views/EmailSenderView.cs
@@ -3,105 +3,17 @@
 public class EmailSenderView : ContentView
 {
     //Fields
-    private readonly string _emailSender;
-    private readonly DateTime _emailSentDate;
     private enum Column { Left = 0, Center = 1, Right = 2 }
-
-    //View components
-    private static Grid ContentGrid = null!;
-    private static Label SentAtLabel = null!;
-    private static Label SenderNameLabel = null!;
-
-    private static IconLabel SenderIcon = null!;
-    private static IconLabel ListenIcon = null!;
-    private static IconLabel RepeatIcon = null!;
-
-    private static VerticalStackLayout? EmailDetailsLayout;
-    private static HorizontalStackLayout? EmailControlsLayout;
 
     //Construction
     public EmailSenderView(string emailSender, DateTime emailSentDate)
     {
-        _emailSender = emailSender;
-        _emailSentDate = emailSentDate;
+        const int fontSize = 30;
+        const double leftColumn = 0.2;
+        const double rightColumn = 0.25;
+        const double centerColumn = 0.55;
 
-        InitializeView();
-    }
-
-    //Initialization
-    protected virtual void InitializeView()
-    {
-        InitializeLabels();
-        InitializeEmailIcons();
-        InitializeLayouts();
-        InitializeContentGrid();
-        
-        Content = ContentGrid;
-    }
-
-    //View component Initialization
-    private static void InitializeEmailIcons()
-    {
-        SenderIcon = new(FontAwesomeIcons.CircleUser);
-
-        ListenIcon = new(FontAwesomeIcons.Headphones)
-        {
-            FontSize = 30
-        };
-        
-        RepeatIcon = new(FontAwesomeIcons.Repeat)
-        {
-            FontSize = 30
-        };
-    }
-    private void InitializeLabels()
-    {
-        SentAtLabel = new()
-        {
-            MaxLines = 1,
-            FontSize = 14,
-            Text = _emailSentDate.ToString("M"),
-            FontAttributes = FontAttributes.Bold
-        };
-
-        SenderNameLabel = new()
-        {
-            MaxLines = 1,
-            FontSize = 16,
-            Text = _emailSender,
-            FontAttributes = FontAttributes.Bold,
-            LineBreakMode = LineBreakMode.TailTruncation,
-        };
-    }
-    private static void InitializeLayouts()
-    {
-        EmailDetailsLayout = new()
-        {
-            Spacing = 5,
-            Children =
-            {
-                SenderNameLabel,
-                SentAtLabel
-            }
-        };
-        EmailControlsLayout = new()
-        {
-            Spacing = 10,
-            Children =
-            {
-                ListenIcon,
-                RepeatIcon
-            }
-        };
-    }
-
-    private static void InitializeContentGrid()
-    {
-        var leftColumn = 0.2;
-        var rightColumn = 0.25;
-        var centerColumn = 0.55;
-
-        ContentGrid = new()
+        Content = new Grid()
         {
             ColumnSpacing = 5,
             ColumnDefinitions =
@@ -112,9 +24,24 @@ public class EmailSenderView : ContentView
             ],
             Children =
             {
-                SenderIcon?.Column(Column.Left),
-                EmailDetailsLayout?.Column(Column.Center),
-                EmailControlsLayout?.Column(Column.Right),
+                new IconLabel(FontAwesomeIcons.CircleUser).Column(Column.Left),
+                new HorizontalStackLayout(){ Spacing = 10,
+                    Children = 
+                    {
+                        new IconLabel(FontAwesomeIcons.Repeat) { FontSize = fontSize },
+                        new IconLabel(FontAwesomeIcons.Headphones) { FontSize = fontSize}
+                    }
+                }.Column(Column.Right),
+
+                new VerticalStackLayout()
+                {
+                    Spacing = 5,
+                    Children =
+                    {
+                        new Label(){ MaxLines = 1, FontSize = 16, Text = emailSender, FontAttributes = FontAttributes.Bold, LineBreakMode = LineBreakMode.TailTruncation},
+                        new Label(){ MaxLines = 1, FontSize = 14, Text = emailSentDate.ToString("M"), FontAttributes = FontAttributes.Bold }
+                    }
+                }.Column(Column.Center)
             }
         };
     }
