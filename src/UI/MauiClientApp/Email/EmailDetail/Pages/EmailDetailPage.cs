caeb168ccbd2d3178f708d8f3fd0b978e6dd52ed
Bongani Elijah Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-08-03 00:35:33 +0200

    Update EmailDetailPage.cs

--- src/UI/MauiClientApp/Email/EmailDetail/Pages/EmailDetailPage.cs
+++ src/UI/MauiClientApp/Email/EmailDetail/Pages/EmailDetailPage.cs
@@ -4,8 +4,6 @@ namespace MauiClientApp.Email.EmailDetail.Pages;
 
 internal class EmailDetailPage : EmailPage<EmailDetailViewModel>
 {
-    private EmailSenderView SenderView = null!;
-
     //Construction
     public EmailDetailPage(EmailDetailViewModel viewModel) : base(viewModel)
     {
@@ -32,19 +30,14 @@ internal class EmailDetailPage : EmailPage<EmailDetailViewModel>
                 .DynamicResource(View.StyleProperty, "EmailSubjectLabel")
                 .Bind(Label.TextProperty, static (EmailDetailViewModel vm) => vm.Subject, mode: BindingMode.OneWay),
 
-            new EmailSenderView(ViewModel.Sender, ViewModel.SentAtDate),
+            new EmailSenderView()
+                .Bind(EmailSenderView.EmailSenderProperty, static (EmailDetailViewModel vm) => vm.Sender, mode: BindingMode.OneWay)
+                .Bind(EmailSenderView.EmailSentDateProperty, static (EmailDetailViewModel vm) => vm.SentAtDate, mode: BindingMode.OneWay),
+            
             new SeparatorLine(),
-
             new Editor(){ IsReadOnly = true }
                 .DynamicResource(StyleProperty, "EmailEditor")
                 .Bind(Editor.TextProperty, static (EmailDetailViewModel vm) => vm.Body, mode: BindingMode.OneWay)
         }
     };
-    //Initialization
-    protected override void OnNavigatedTo(NavigatedToEventArgs args)
-    {
-        base.OnNavigatedTo(args);
-
-        InitializeEmailPage();
-    }
 }
