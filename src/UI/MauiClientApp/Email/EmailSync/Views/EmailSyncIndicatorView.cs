1de2a31499693cac12989b79d4747565ac8142a4
Bongani Elijah Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-08-03 18:32:49 +0200

    Update EmailSyncIndicatorView.cs

--- src/UI/MauiClientApp/Email/EmailSync/Views/EmailSyncIndicatorView.cs
+++ src/UI/MauiClientApp/Email/EmailSync/Views/EmailSyncIndicatorView.cs
@@ -4,63 +4,41 @@ namespace MauiClientApp.Email.EmailSync.Views;
 
 public class EmailSyncIndicatorView : ContentView
 {
-    //View components
-    private Label HeaderLabel = null!;
-    private Label WaiterLabel = null!;
-    private SKLottieView SyncLottieView = null!;
-
     //Construction
     public EmailSyncIndicatorView()
     {
-        InitializeLabels();
-        InitializeLottieView();
-
         Content = new VerticalStackLayout()
         {
             Spacing = 25,
             VerticalOptions = LayoutOptions.Center,
             Children =
             {
-                SyncLottieView,
-                HeaderLabel, 
-                WaiterLabel,
+                new SKLottieView()
+                {
+                    RepeatCount = -1,
+                    WidthRequest = 300,
+                    HeightRequest = 300,
+                    Source = new SKFileLottieImageSource()
+                    {
+                        File = "EmailSync.json"
+                    }
+                },
+                new Label()
+                {
+                    FontSize = 22,
+                    Text = "Syncing emails",
+                    FontAttributes = FontAttributes.Bold,
+                    HorizontalTextAlignment = TextAlignment.Center,
+                },
+                new Label()
+                {
+                    MaxLines = 1,
+                    FontSize = 18,
+                    Text = "Please wait...",
+                    FontAttributes = FontAttributes.Bold,
+                    HorizontalTextAlignment = TextAlignment.Center,
+                }
             }
         };
     }
-
-    //View component Initialization
-    private void InitializeLabels()
-    {
-        HeaderLabel = new Label()
-        {
-            FontSize = 22,
-            Text = "Syncing emails",
-            FontAttributes = FontAttributes.Bold,
-            HorizontalTextAlignment = TextAlignment.Center,
-        };
-
-        WaiterLabel = new Label()
-        {
-            MaxLines = 1,
-            FontSize = 18,
-            Text = "Please wait...",
-            FontAttributes = FontAttributes.Bold,
-            HorizontalTextAlignment = TextAlignment.Center,
-        };
-    }
-
-    private void InitializeLottieView()
-    {
-        SyncLottieView = new SKLottieView()
-        {
-            RepeatCount = -1,
-            WidthRequest = 300,
-            HeightRequest = 300,
-            Source = new SKFileLottieImageSource()
-            {
-                File = "EmailSync.json"
-            }
-        };
-    }
-
 }
