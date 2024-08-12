eaa8e716cb950a4db9fbaf33065fda84d3ecdf74
Bongani Elijah Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-08-12 21:50:10 +0200

    Update ContactDetailViewModel.cs

--- src/UI/MauiClientApp/Contacts/ContactDetail/ViewModels/ContactDetailViewModel.cs
+++ src/UI/MauiClientApp/Contacts/ContactDetail/ViewModels/ContactDetailViewModel.cs
@@ -1,5 +1,27 @@
 ﻿namespace MauiClientApp.Contacts.ContactDetail.ViewModels;
 
-internal class ContactDetailViewModel(IMediator mediator) : ViewModel(mediator, false)
+internal partial class ContactDetailViewModel(IMediator mediator) : ViewModel(mediator, false)
 {
+    //Properties
+    private ContactDto SelectedContact { get; set; } = null!;
+
+    //View elements
+    [ObservableProperty] private string contactName = string.Empty;
+    [ObservableProperty] private string contactEmail = string.Empty;
+    
+    //Navigation
+    public void ApplyQueryAttributes(IDictionary<string, object> query)
+    {
+        SelectedContact = (ContactDto)query[nameof(ContactDto)];
+        
+        ContactName = SelectedContact.Name;
+        ContactEmail = SelectedContact.EmailAddress;
+    }
+
+    //Commands
+    [RelayCommand]
+    public async Task SaveContact()
+    {
+        //var command = CreateCont
+    }
 }
