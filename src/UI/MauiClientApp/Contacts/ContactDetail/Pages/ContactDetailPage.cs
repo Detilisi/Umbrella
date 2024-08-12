587b32174880504c0c4c217d049ac978d7dd304b
Bongani Elijah Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-08-12 21:17:44 +0200

    Create ContactDetailPage.cs

---
+++ src/UI/MauiClientApp/Contacts/ContactDetail/Pages/ContactDetailPage.cs
@@ -0,0 +1,25 @@
+﻿namespace MauiClientApp.Contacts.ContactDetail.Pages;
+
+internal class ContactDetailPage : Page<ContactDetailViewModel>
+{
+    public ContactDetailPage(ContactDetailViewModel viewModel): base(viewModel)
+    {
+        var nameEntry = new Entry { Placeholder = "Name:" }
+            .DynamicResource(StyleProperty, "SignUpEntry");
+
+        var emailEntry = new Entry { Placeholder = "Email:" }
+            .DynamicResource(StyleProperty, "SignUpEntry");
+
+        //Size = new Size();
+        Content = new VerticalStackLayout()
+        {
+            Spacing = 25,
+            VerticalOptions = LayoutOptions.Center,
+            Children =
+            {
+                nameEntry,
+                emailEntry
+            }
+        };
+    }
+}
