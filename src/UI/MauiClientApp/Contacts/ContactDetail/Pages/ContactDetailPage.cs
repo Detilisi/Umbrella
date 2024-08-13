64117b8e161c272c97bc4662774a3ad577948fda
Bongani Elijah Detilisi <162840178+Detilisi@users.noreply.github.com>
2024-08-13 22:25:03 +0200

    Update ContactDetailPage.cs

--- src/UI/MauiClientApp/Contacts/ContactDetail/Pages/ContactDetailPage.cs
+++ src/UI/MauiClientApp/Contacts/ContactDetail/Pages/ContactDetailPage.cs
@@ -6,12 +6,29 @@ internal class ContactDetailPage : Page<ContactDetailViewModel>
     {
         Title = "Contact";
 
+        ToolbarItems.Add(new ToolbarItem
+        {
+            IconImageSource = new FontImageSource
+            {
+                Size = 30,
+                FontFamily = "FontAwesomeSolid",
+                Glyph = FontAwesomeIcons.TrashCan,
+            },
+            //Command = new Command(async () => await ViewModel.ReplyEmailCommand.ExecuteAsync(null))
+        });
+
+
+        InitializeContent();
+    }
+
+    private void InitializeContent()
+    {
         var nameEntry = new Entry { Placeholder = "Name:" }.DynamicResource(StyleProperty, "SignUpEntry");
         nameEntry.Bind(Entry.TextProperty, static (ContactDetailViewModel vm) =>
             vm.ContactName, static (ContactDetailViewModel vm, string text) => vm.ContactName = text);
 
         var emailEntry = new Entry { Placeholder = "Email:" }.DynamicResource(StyleProperty, "SignUpEntry");
-        emailEntry.Bind(Entry.TextProperty, static (ContactDetailViewModel vm) => 
+        emailEntry.Bind(Entry.TextProperty, static (ContactDetailViewModel vm) =>
             vm.ContactEmail, static (ContactDetailViewModel vm, string text) => vm.ContactEmail = text);
 
         var saveButton = new Button()
